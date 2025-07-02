51cbe3ac99151de799c1ea15af3bd23199ce7428
Дикаев Рахман Расулович <rahman.corporative@gmail.com>
2025-07-02 14:57:47 +0300

    Настройка docker-compose

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Allows access from outside the container
+        port: 5173,     // Default Vite development server port
+        hmr: {
+            clientPort: 5173,
+            host: 'localhost' // Or your Docker host IP if needed
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
